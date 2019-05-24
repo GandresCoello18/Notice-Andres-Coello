74b936f96bfeddc701763e875adca40baf7c559d
Andres <45317893+GandresCoello18@users.noreply.github.com>
2019-05-23 22:45:31 -0500

    Add files via upload
    cambiar archivo codigo

--- src/componentes/codigo.js
+++ src/componentes/codigo.js
@@ -1,22 +1,35 @@
 import React from 'react';
+import Json from './json-codigo.json';
+import './css/tareas.css';
 
 class Codigo extends React.Component{
+
+    componentDidMount(){
+        if(!localStorage.getItem("acceso")){
+            window.location.href="https://gandrescoello18.github.io/social-students/";
+        }
+    }
+
     render(){
         return(
             <div className="row justify-content-center">
-                <div className="col-12 mt-2 col-md-8">
+                {Json.codigos.map(valor => (
+                    <div className="col-12 mt-2 col-md-8" key={valor.id}>
+                    <a href={valor.direccion} target="_blanck">
                     <article className="p-3 articulo">
                         <div className="row">
                             <div className="col-9">
-                                <h5 className="p-1">Simulacion de carrito de compras</h5>
-                                <p className="p-1">The crypto markets have incurred overwhelmingly bullish price action over the past several weeks that appears to have confirmed many analyst’s belief that $3,200 truly is a long-term bottom for Bitcoin (BTC). The recent price action has drastically shifted ov…</p>
+                                <h5 className="p-1">{valor.titulo}</h5>
+                                <p className="p-1">{valor.descripcion}</p>
                             </div>
                             <div className="col-3">
                                 <img src="images/coding.svg" className="img-fluid"/>
                             </div>
                         </div>
                     </article>
+                    </a>
                 </div>
+                ))}
             </div>
         )
     }
