15751b14b49a8bdd815caa0a28012c74acd16cc0
Andres <45317893+GandresCoello18@users.noreply.github.com>
2019-05-23 21:50:02 -0500

    Add files via upload
    componentes y pages

---
+++ src/componentes/cuadro_registro.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import Modal from './modal';
+import Json from './correos.json';
+import './css/login.css';
+
+class CuadroRegistro extends React.Component{
+    state = {
+        imagen1: 'images/smartphone.svg',
+        imagen2: 'images/notification.svg',
+        imagen3: 'images/user.svg',
+        text_modal: '',
+        id: Json.correos[0].id
+    }
+
+    componentDidMount(){
+        if(this.verificar(localStorage.getItem("acceso")) === true ){
+            setTimeout( () => {
+                alert('Bienvenido, presiona aceptar');
+                var pagina="general";
+                window.location.href=pagina;
+            },2000);
+        }
+        //console.log(Json.correos);
+    }
+
+    verificar = (memoria) => {
+        for(var i=0; i<Json.correos.length; i++){
+            if(memoria == Json.correos[i].direccion){
+               return true; 
+            }
+        }
+    }
+
+     registro = (e) => {
+         e.preventDefault();
+        var correo = document.getElementById('correo');
+        if(correo.value === '' || correo.value.indexOf('@') === -1 ){
+            this.setState({text_modal: 'Campos vacios o No escribio un correo electronico'});
+        }else{
+            this.setState({text_modal: 'Gracias por preferir Social Students...'});
+            localStorage.setItem("acceso", correo.value);
+            setTimeout(() =>{
+                var pagina="general";
+                window.location.href=pagina;
+            },2000)
+        }   
+    }
+
+    render(){
+        return(
+            <div className="cuadro-login" id="root2">
+            <Modal isOpen={true}>{this.state.text_modal}</Modal>
+                <div className="row align-itens-center">
+                    <div className="col-12">
+                        <div className="logo">
+                            <img src={this.state.imagen1} className="icon-mobile" width="38" height="38" />
+                        </div> 
+                        <h5 className="text-center subtitulo mt-4">Social Students</h5>
+                        <div className="row justify-content-around mt-4">
+                            <div className="col-6">
+                                <div className="ovalo">
+                                <img src={this.state.imagen2} className="icon-notification" width="20" height="20" />
+                                <p className="text-center">13 Actividades</p>
+                                </div>
+                            </div>
+                            <div className="col-6">
+                                <div className="ovalo">
+                                <img src={this.state.imagen3} className="icon-notification" width="20" height="20" />
+                                <p className="text-center">{this.state.id} Miembros</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row justify-content-center mt-4">
+                            <div className="col-10">
+                                <form>
+                                    <input type="email" id="correo" required="" className="form-control  text-center" placeholder="@ - Correo Electronico"/>
+                                    <button type="submit" data-toggle="modal" data-target="#cuadro" className="btn color-btn mt-2 form-control" onClick={this.registro}>Registrarme</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CuadroRegistro;
---
+++ src/componentes/nav.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './css/nav.css';
+
+class Nav extends React.Component{
+    menu = () => {
+        var refe = document.querySelector('.cuadro-menu');
+        if(refe.style.display === 'block'){
+            refe.style.display = 'none';
+        }else{
+            refe.style.display = 'block';
+        }
+    }
+    render(){
+        var estilos = {
+            link: {
+                color: "white",
+            }
+        }
+        return(
+            <nav className="row navegacion">
+                <div className="col-6">
+                <h5 className="text-md-center mt-3">Social Students <img src="images/smartphone.svg" width="35" height="35" /></h5>
+                </div>
+                <div className="col-6" onClick={this.menu}>
+                <p className="text-right mt-3"><img src="images/menu.svg" width="30" height="30" /></p>
+                    <div className="row justify-content-end">
+                        <div className="col col-md-6 col-lg-4 cuadro-menu">
+                            <ul className="nav flex-column"> 
+                                <Link to="general"><li className="nav-item p-1">General</li></Link>
+                                <Link to="tareas"><li className="nav-item p-1">Tareas y Trabajo</li></Link>
+                                <Link to="codigo"><li className="nav-item p-1">Codigos</li></Link>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Nav;
